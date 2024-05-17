d7d72d9ca9692b374560ad0bf60055450af1ac68
Paras Singhal <parassinghal2310@gmail.com>
2024-05-17 09:44:38 +0530

    Initial Commit

---
+++ AutiCare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutiCare
+//
+//  Created by Batch-2 on 17/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
