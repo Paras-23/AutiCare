bd1e6a5a79df36484d99efa23eeb1ea7c57b6a5b
Madhav Verma <madhavverma.hll@gmail.com>
2024-05-17 12:36:14 +0530

    created Assesment view Controller , question View Controller and data model

---
+++ AutiCare/Progress/QuestionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionTableViewCell.swift
+//  AutiCare
+//
+//  Created by Batch-1 on 17/05/24.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
