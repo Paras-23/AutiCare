39f76502387b983a8b4a2b21dcb71a0ce7c460bd
Sudhanshu Kumar <sudhanshukumar07777@gmail.com>
2024-05-27 11:43:31 +0530

    Learning Page Interface Designed

--- AutiCare/Learning/LearningDataModel.swift
+++ AutiCare/Learning/LearningDataModel.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
-struct GamesImages {
-    var nameID : String
+struct Games{
+    var imageName:String
+}
+struct Sessions{
+    var imageName:String
+}
+struct Worksheets{
+    var imageName:String
+}
+
+class DataModel{
+    static var games:[Games] = [Games(imageName: "Designer-4"),Games(imageName: "Designer-5"),Games(imageName: "Designer-6")]
+    static var sessions:[Sessions] = [Sessions(imageName: "Designer-4"),Sessions(imageName: "Designer-4"),Sessions(imageName: "Designer-4")]
+    static var worksheets:[Worksheets] = [Worksheets(imageName: "Designer-4"),Worksheets(imageName: "Designer-4"),Worksheets(imageName: "Designer-4")]
+    static var sectionHeader:[String] = ["Games","Sessions","Worksheets"]
 }
