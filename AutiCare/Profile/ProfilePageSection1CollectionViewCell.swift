d8c02640220d9c72b0789a85656883d8c973456a
Sudhanshu Kumar <sudhanshukumar07777@gmail.com>
2024-05-29 17:45:12 +0530

    fixed the errors

---
+++ AutiCare/Profile/ProfilePageSection1CollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FirstProfilePageCollectionViewCell.swift
+//  AutiCare
+//
+//  Created by Batch-1 on 28/05/24.
+//
+
+import UIKit
+
+class ProfilePageSection1CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBAction func chevronButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+}
---
+++ AutiCare/Profile/ProfilePageSection2CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilePageSection2CollectionViewCell.swift
+//  AutiCare
+//
+//  Created by Batch-1 on 28/05/24.
+//
+
+import UIKit
+
+class ProfilePageSection2CollectionViewCell: UICollectionViewCell {
+    
+
+    @IBOutlet var logoImageView: UIImageView!
+    
+    @IBOutlet var logoName: UILabel!
+    
+}
