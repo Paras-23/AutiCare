d1208e21a2abeaa2b769cfe973f816ff6e6493ba
code-with-sourav <souravkumarsingh624@gmail.com>
2024-06-01 17:46:29 +0530

    again commiting the same progress

--- AutiCare/Community/ChatSection/Controllers/conversationViewController.swift
+++ AutiCare/Community/ChatSection/Controllers/conversationViewController.swift
@@ -1,29 +1,63 @@
 //
-//  conversationViewController.swift
-//  AutiCare
+//  ViewController.swift
+//  ChatApp
 //
-//  Created by sourav_singh on 01/06/24.
+//  Created by sourav_singh on 21/05/24.
 //
 
 import UIKit
+import FirebaseAuth
 
-class conversationViewController: UIViewController {
-
+class ConversationsViewController: UIViewController {
+    
+    // private let spinner = JGProgressHUD(style: .dark)
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self,forCellReuseIdentifier: "cell"
+        )
+        return table
+    } ()
+    
+    //    private let noConversationsLabel: UILabel = {
+    //        let label = UILabel()
+    //        label.text = "No Conversation!"
+    //        label.textAlignment = .center
+    //        label.textColor = .gray
+    //        label.font = .systemFont(ofSize: 21, weight: .medium)
+    //        label.isHidden = true
+    //        return label
+    //    }
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//        view.addSubview(tableView)
+//        setuptableView()
+        //                fetchConversations()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
     }
-    */
-
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
 }
+        
+//            private func setuptableView() {
+//
+//                tableView.delegate = self
+//                tableView.dataSource = self
+//            }
+        
