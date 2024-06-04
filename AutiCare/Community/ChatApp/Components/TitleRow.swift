47e8a4d26175c4e502f3834dfa852308e8f8c763
Sudhanshu Kumar <sudhanshukumar07777@gmail.com>
2024-06-04 21:03:35 +0530

    integrated the chat functionality

--- AutiCare/Community/ChatApp/Components/TitleRow.swift
+++ AutiCare/Community/ChatApp/Components/TitleRow.swift
@@ -31,12 +31,6 @@ struct TitleRow: View {
                     .foregroundColor(.gray)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Image(systemName: "phone.fill")
-                .foregroundColor(.gray)
-                .padding(10)
-                .background(.white)
-                .cornerRadius(50)
         }
         .padding()
     }
