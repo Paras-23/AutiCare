2d200b7d6bb2be91e4a21651ad3021f16edb1f22
code-with-sourav <souravkumarsingh624@gmail.com>
2024-06-05 11:11:13 +0530

    Progress Page is completed

---
+++ AutiCare/Progress/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  AutiCare
+//
+//  Created by sourav_singh on 05/06/24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
---
+++ AutiCare/Progress/HostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HostViewController.swift
+//  AutiCare
+//
+//  Created by sourav_singh on 05/06/24.
+//
+
+import UIKit
+
+class HostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
