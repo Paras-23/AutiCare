6e750fc49843448612676aee1d3cabb140383391
Paras Singhal <parassinghal2310@gmail.com>
2024-06-05 11:25:37 +0530

    Connected Chat Page to Community Section

---
+++ AutiCare/Community/ChatSectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatSectionTableViewCell.swift
+//  AutiCare
+//
+//  Created by Batch-2 on 05/06/24.
+//
+
+import UIKit
+
+class ChatSectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
