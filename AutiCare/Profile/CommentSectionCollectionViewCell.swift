2e1020a926a59c2cccd140d0e502ca55a94c42a8
Paras Singhal <parassinghal2310@gmail.com>
2024-06-06 11:51:02 +0530

    Updated Profile Interface

---
+++ AutiCare/Profile/CommentSectionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CommentSectionCollectionViewCell.swift
+//  AutiCare
+//
+//  Created by Batch-2 on 06/06/24.
+//
+
+import UIKit
+
+class CommentSectionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
