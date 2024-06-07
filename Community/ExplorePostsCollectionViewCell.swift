6dac6b4b9cd53309c55cc8aadc64b47d6895bc63
Madhav Verma <119183615+vermamadhav@users.noreply.github.com>
2024-06-07 17:53:03 +0530

    all users posts shows on explore

---
+++ Community/ExplorePostsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExplorePostsCollectionViewCell.swift
+//  AutiCare
+//
+//  Created by Batch-1 on 07/06/24.
+//
+
+import UIKit
+
+class ExplorePostsCollectionViewCell: UICollectionViewCell {
+    
+}
